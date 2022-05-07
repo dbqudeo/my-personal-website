ec0853bcc0b77c66d3d4076ea69965d2ab8c96a7
byeongdae yu <brianbdyu@gmail.com>
2022-05-07 17:17:00 -0400

    finish nav bar

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+const toggleBtn = document.querySelector('.navbar_toggleBtn');
+const menu = document.querySelector('.navbar_menu');
+const icons = document.querySelector('.navbar_icons');
+
+/*toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active')
+    icons.classList.toggle('active')
+});
+*/
+
+/*const h2 = document.createElement("h2");
+h2.textContent = "Hi, my name is";
 document.querySelector("body").appendChild(h2);
+*/
